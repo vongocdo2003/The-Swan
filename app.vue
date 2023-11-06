b63c6ece29a05270272b061983dcc337ef58eb82
vongocdo2003 <145606358+vongocdo2003@users.noreply.github.com>
2023-11-07 02:51:52 +0700

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-3 gap-4">
+  <div class="bg-slate-400">01</div>
+  <div class="bg-slate-400">02</div>
+  <div class="bg-slate-400">03</div>
+  <div class="col-span-2 bg-slate-400">04</div>
+  <div class="bg-slate-400">05</div>
+  <div class="bg-slate-400">06</div>
+  <div class="col-span-2 bg-slate-400">07</div>
+</div>
+</template>
